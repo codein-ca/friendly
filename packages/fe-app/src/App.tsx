e1d12fc183d555b107c942d34113d9ed9a8cfe38
Daniel Santos <daniel@codein.ca>
2020-03-15 18:10:00 -0400

    fix/make-storybook-same-as-main-app

--- packages/fe-app/src/App.tsx
+++ packages/fe-app/src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-// import Link from "";
+import Link from "@codein/fe-app/src/components/Link";
 
 function App() {
   return (
     <div className="App">
       <h1>Test linking a Storybook component</h1>
       <a href="#not-the-one">Should render below this one</a>
-      {/*<Link linkHREF="google.com" linkText="Google" />*/}
+      <Link linkHREF="google.com" linkText="Google" />
     </div>
   );
 }
