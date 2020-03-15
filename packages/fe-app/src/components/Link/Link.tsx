737b8268a0c4472e22ad53835686bb41df0abb90
Daniel Santos <daniel@codein.ca>
2020-03-15 18:43:29 -0400

    feat: issue-9/add-emotion

--- packages/fe-app/src/components/Link/Link.tsx
+++ packages/fe-app/src/components/Link/Link.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import styled from "@emotion/styled";
+
 type LinkType = {
   linkHREF: string;
   linkText: string;
 };
 
+const LinkStyled = styled.a`
+  text-decoration: none;
+  background-color: rgba(221, 206, 29, 0.25);
+  box-shadow: 0 0 0.5em rgba(221, 206, 29, 0.5);
+  padding: 0 0.5em;
+  color: fuchsia;
+  white-space: pre;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  &:after {
+    content: " 📌";
+    font-size: x-small;
+  }
+`;
+
 const Link = ({ linkHREF, linkText }: LinkType) => {
   linkText = linkText ? linkText : "[link]";
-  return <a href={linkHREF}>{linkText}</a>;
+  return <LinkStyled href={linkHREF}>{linkText}</LinkStyled>;
 };
 
 export default Link;
