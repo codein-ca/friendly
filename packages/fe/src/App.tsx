0622be35bda88780dc2391ce1707dc55e48ffabe
Daniel Santos <daniel@codein.ca>
2020-03-15 02:32:41 -0400

    clean up of CRA boilerplate

--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import Link from "";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test linking a Storybook component</h1>
+      <a href="#not-the-one">Should render below this one</a>
+      {/*<Link linkHREF="google.com" linkText="Google" />*/}
     </div>
   );
 }
